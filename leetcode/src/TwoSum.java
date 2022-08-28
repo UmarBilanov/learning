eef90e454100ecc8dbec8edb87e4de7daa3c8e1c
umar <umar.uranuulu@gmail.com>
2022-08-28 13:26:25 +0600

    two sum

---
+++ leetcode/src/TwoSum.java
@@ -0,0 +1,42 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+
+    public static int[] twoSum(int[] nums, int target) {
+        int[] res = new int[2];
+        int len = nums.length;
+        for (int i = 0; i < len; i++) {
+            for (int j = i+1; j < len; j++) {
+                if (nums[i] + nums[j] == target) {
+                    res[0] = i;
+                    res[1] = j;
+                    return res;
+                }
+            }
+        }
+        return res;
+    }
+
+    public static int[] twoSumWithMap(int[] nums, int target) {
+        int[] res = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])){
+                res[1] = i;
+                res[0] = map.get(target - nums[i]);
+                return res;
+            }
+            map.put(nums[i], i);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2,7,11,15};
+        int[] res = twoSumWithMap(nums, 9);
+        for (int i:res) {
+            System.out.println(i);
+        }
+    }
+}
