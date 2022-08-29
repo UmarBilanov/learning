d88b5ed2919b63ab8af2ee122c8c9030b3a6317d
umar <umar.uranuulu@gmail.com>
2022-08-29 22:23:28 +0600

    add two number

--- src/leetcode/AddTwoNumbers.java
+++ src/leetcode/AddTwoNumbers.java
@@ -2,27 +2,31 @@ package leetcode;
 
 public class AddTwoNumbers {
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode res = new ListNode(l1.val + l2.val);
-        while (l1.next.next == null && l2.next.next == null) {
+        ListNode res = new ListNode();
+        System.out.println("res val: " + res.val);
+        while (l1.next != null && l2.next != null) {
             System.out.println("res val: " + res.val);
+            res = new ListNode(l1.val + l2.val, res);
             l1 = l1.next;
             l2 = l2.next;
-            res = new ListNode(l1.val + l2.val, res);
         }
         return res;
     }
 
     public static void main(String[] args) {
+        int[] il1 = {2, 4, 3};
+        int[] il2 = {5, 6, 4};
+        ListNode l1 = new ListNode(il1[0]);
+        ListNode l2 = new ListNode(il2[0]);
+        for (int i = 1; i < 3; i++) {
+            l1 = new ListNode(il1[i], l1);
+            l2 = new ListNode(il2[i], l2);
+        }
+
+        addTwoNumbers(l1, l2);
     }
 }
 
-class ListNode {
-      int val;
-      ListNode next;
-      ListNode() {}
-      ListNode(int val) { this.val = val; }
-      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-}
 
 //Input: l1 = [2,4,3], l2 = [5,6,4]
 //        Output: [7,0,8]
